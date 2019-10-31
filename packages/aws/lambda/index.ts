78bf873af2236741e37062dcac6a7c865cb82adf
yamatatsu <the.kinnikuman@gmail.com>
2019-10-31 23:28:10 +0900

    test deploying lib

--- packages/aws/lambda/index.ts
+++ packages/aws/lambda/index.ts
@@ -1,8 +1,9 @@
 import { Handler } from "aws-lambda";
+import { getNum } from "./lib";
 
 console.info("Loading function");
 const handler: Handler = async () => {
-  return { statusCode: 201, body: "2" };
+  return { statusCode: 201, body: getNum() };
 };
 
 exports.handler = handler;
---
+++ packages/aws/lambda/lib.ts
@@ -0,0 +1,1 @@
+export const getNum = () => 3;
