1db75f5a32406fa42442599a619ac84f6b50a85b
yamatatsu <the.kinnikuman@gmail.com>
2019-11-05 12:05:20 +0900

    lambdas

--- aws/stacks/Receptron.ts
+++ aws/stacks/Receptron.ts
@@ -1,20 +1,52 @@
 import cdk = require("@aws-cdk/core");
 import apigateway = require("@aws-cdk/aws-apigateway");
 import lambda = require("@aws-cdk/aws-lambda");
+import dynamodb = require("@aws-cdk/aws-dynamodb");
+import { DynamoEventSource } from "@aws-cdk/aws-lambda-event-sources";
 
 export class Receptron extends cdk.Stack {
   constructor(parent: cdk.App, id: string, props?: cdk.StackProps) {
     super(parent, id, props);
 
-    const handler = new lambda.Function(this, "Lambda", {
-      code: new lambda.AssetCode("../packages/lambda/dist"),
-      handler: "index.handler",
-      runtime: lambda.Runtime.NODEJS_10_X,
-      environment: {},
+    const callTable = new dynamodb.Table(this, "CallTable", {
+      tableName: "Call",
+      partitionKey: {
+        name: "organizationId",
+        type: dynamodb.AttributeType.STRING,
+      },
+      sortKey: {
+        name: "timestamp",
+        type: dynamodb.AttributeType.STRING,
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      stream: dynamodb.StreamViewType.NEW_IMAGE,
     });
 
-    const api = new apigateway.RestApi(this, "RestApi");
+    const code = new lambda.AssetCode("../packages/lambda/dist");
+    const createLambda = lambdaFactory(this, code);
+    const healthCheck = createLambda("healthCheck");
+    const createCall = createLambda("createCall");
+    const callStream = createLambda("callStream");
+
+    callStream.addEventSource(
+      new DynamoEventSource(callTable, {
+        startingPosition: lambda.StartingPosition.TRIM_HORIZON,
+      }),
+    );
+
+    const api = new apigateway.RestApi(this, "Receptron");
+    api.root.addMethod("get", new apigateway.LambdaIntegration(healthCheck));
     const callApi = api.root.addResource("calls", {});
-    callApi.addMethod("post", new apigateway.LambdaIntegration(handler));
+    callApi.addMethod("post", new apigateway.LambdaIntegration(createCall));
   }
 }
+
+const lambdaFactory = (scope: cdk.Construct, code: lambda.Code) => (
+  id: string,
+) =>
+  new lambda.Function(scope, id, {
+    handler: `index.${id}`,
+    code,
+    runtime: lambda.Runtime.NODEJS_10_X,
+    environment: {},
+  });
